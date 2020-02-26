91dc7696d4408795480d5c8ca5e318dfd0d901d4
Ivan Lozano <ivanlozmar@gmail.com>
2020-02-26 17:49:40 -0300

    Added orders controllers with actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.where(user: current_user)
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = Order.new(user: current_user.id, status: "pending")
+
+    redirect_to @order_id
+  end
+
 end
