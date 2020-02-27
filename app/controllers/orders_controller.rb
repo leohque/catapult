5620f8db51d23d1b3105e653a50b39301d3849ae
Ivan Lozano <ivanlozmar@gmail.com>
2020-02-27 17:15:49 -0300

    working on create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,20 +2,57 @@ class OrdersController < ApplicationController
 
   def index
     # @orders = Order.where(user: current_user)
-    @orders = current_user.orders
+    @orders = current_user.orders.order(created_at: :desc)
+    # @orders = policy_scope(Order).order(created_at: :desc) -> this would give us the same result
   end
 
   def show
     @order = Order.find(params[:id])
+
+    authorize @order
+  end
+
+  def new
+    @order = Order.new
   end
 
   def create
-    @order = Order.new(user: current_user.id, status: "pending")
-    @art_order = ArtOrder.new
+    raise
+    @art = 1
+    if current_user.orders.where(status: 'open').any?
+      @order = current_user.find_by(status: 'open')
+      @art_order = ArtOrder.new(order: @order)
+      @art_order.art = @art
+      @art_order.price = @art.price
+      @art_order.save
+      redirect_to arts_path
+    else
+      @order = Order.new(user: current_user, status: "open")
+      @art_order = ArtOrder.new
+
+    end
+# check if there is any open order
+# if not, create a new order with the art
+# else add the art ot the open order
+
+
+
+
+
+
+
     @art_order.order = @order
     @art_order.art = y
     @order.art_order = @art_order
     redirect_to @order_id
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 
 end
--- app/policies/order_policy.rb
+++ app/policies/order_policy.rb
@@ -1,7 +1,27 @@
 class OrderPolicy < ApplicationPolicy
-  class Scope < Scope
+  class Scope < Scope # Scope represents the class Oder
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    record.user == user # record represents the specific object
+  end
+
+  def create?
+    index?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
   end
 end
