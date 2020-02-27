1a5fd9667baad33ae7e4e0309e5068672f1c3cc1
Ivan Lozano <ivanlozmar@gmail.com>
2020-02-27 11:42:15 -0300

    add orders controller, model and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.where(user: current_user)
-    # @orders = current_user.orders
+    # @orders = Order.where(user: current_user)
+    @orders = current_user.orders
   end
 
   def show
@@ -11,7 +11,10 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(user: current_user.id, status: "pending")
-
+    @art_order = ArtOrder.new
+    @art_order.order = @order
+    @art_order.art = y
+    @order.art_order = @art_order
     redirect_to @order_id
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :art_orders
   has_many :arts, through: :art_orders
+
+  STATUS = ['open', 'cancelled', 'confirmed']
 end
