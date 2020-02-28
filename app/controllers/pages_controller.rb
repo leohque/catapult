877e14dd324b74944372704add500a9b08792c50
leo martinez <leohque@gmail.com>
2020-02-28 14:06:45 -0300

    Edited card component + functionality for card & map in controllers + moved map to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @users = User.geocoded
+    @markers = @users.map do |user|
+       {
+        lat: user.latitude,
+        lng: user.longitude,
+        infoWindow: render_to_string(partial: "users/info_window", locals: { user: user }),
+        image_url: helpers.asset_url('cat.png')
+      }
+    end
   end
 
   def profile
