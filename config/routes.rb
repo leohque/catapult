cf85916932cf12a03abccf2c34b326b70bb938c3
leo martinez <leohque@gmail.com>
2020-03-02 18:05:02 -0300

    Edited forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/', to: 'pages#home'
 
-  get '/profile', to: 'pages#profile'
   resources :arts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, except: :update
   resources :art_orders, only: :destroy
 
