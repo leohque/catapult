3e282e9c42ec394c3ed472830ac0ca60407eed41
Clelia Bailly <cleliabailly@gmail.com>
2020-03-03 12:12:16 -0300

    add the username on the sign up and display on the profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   has_one_attached :photo
-
+  validates :username, uniqueness: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
