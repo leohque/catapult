309a56631656b464886d9b4e434a32fc75ac6805
leo martinez <leohque@gmail.com>
2020-03-03 11:48:27 -0300

    Migration to default quantity to 1 + edited seeds

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_233540) do
     t.string "description"
     t.string "category"
     t.integer "price"
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
